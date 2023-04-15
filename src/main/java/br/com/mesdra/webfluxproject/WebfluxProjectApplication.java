515d1dd233dfec3ebffb3c01a3e75f78693ba99b
vinicius <vinicavalari@hotmail.com>
2023-04-14 22:23:51 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/mesdra/webfluxproject/WebfluxProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.mesdra.webfluxproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mesdra/webfluxproject/WebfluxProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mesdra.webfluxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
